8dd0bca7f30de4c0d2880d2a91e20e788108d7a2
Eugene Papchinskiy <paphinskii@yandex.ru>
2026-02-01 01:09:41 +0300

    refactor: move app wiring to main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"URL-Shortener/internal/config"
+	"URL-Shortener/internal/handler"
+	"URL-Shortener/internal/repos"
 	"URL-Shortener/internal/routers"
+	"URL-Shortener/internal/service"
 )
 
 func main() {
 	cfg := config.Load()
 
-	router := routers.SetupRouter()
+	repo := repos.NewUrlRepos()
+	service := service.NewUrlService(repo)
+	handler := handler.NewUrlHandler(service, cfg.BaseURL)
+
+	router := routers.SetupRouter(handler)
 	router.Run(":" + cfg.Port)
 }
